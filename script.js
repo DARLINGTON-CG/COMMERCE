591ae92a85374d1f980ac77d64da8a77a816fda8
Darlington <yeboahdarlingtonk@gmail.com>
2023-01-02 19:25:59 +0100

    shoe shop desktop version finished

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 images = document.getElementsByClassName("thumb-image");
 mainImage = document.getElementById("mainImage");
+addToCart = document.getElementById("addToCart");
+
 let cartBasket = document.getElementById("cartButton");
 let cartContainer = document.getElementById("cartContainer");
 let imageAvatar = document.getElementById("imageAvatar");
 let totalPrice = document.getElementById("totalPrice");
 let totalSelected = document.getElementById('totalSelected');
+let totalItemsTip = document.getElementById("totalItemsTip");
+let totalItemsDiv = document.getElementById("totalItemsDiv");
+
 
 let totalQuantity = document.getElementById("totalQ");
 incrementQuantity = document.getElementById("incrementQ");
@@ -15,6 +20,38 @@ emptyCart = document.getElementById("emptyCart");
 
 
 const imagePaths = ["./assets/image-product-1.jpg", "./assets/image-product-2.jpg", "./assets/image-product-3.jpg", "./assets/image-product-4.jpg"]
+
+addToCart.addEventListener("click", () => {
+    totalItemsTip.innerText = totalSelected.innerHTML;
+    totalItemsDiv.style.visibility = "visible";
+    totalSelected.innerText = totalQuantity.innerText;
+    totalPrice.innerText =  "$"+ (parseInt(totalQuantity.innerText) * 125).toString();
+        
+    if(parseInt(totalSelected.innerText.toString()) <= 0 )
+    {
+        if(!filledCart.classList.contains("removed-content"))
+        {
+            filledCart.classList.add("removed-content");
+        }
+        if(emptyCart.classList.contains("removed-content"))
+        {
+            emptyCart.classList.remove("removed-content");
+        }
+    }
+    else
+    {
+        if(!emptyCart.classList.contains("removed-content"))
+        {
+            emptyCart.classList.add("removed-content");
+        }
+
+        if(filledCart.classList.contains("removed-content"))
+        {
+            filledCart.classList.remove("removed-content");
+        }
+    } 
+
+})
 
 imageAvatar.addEventListener("mouseenter",() =>{
     let avatarContainer = document.getElementById("avatarContainer");
@@ -31,16 +68,21 @@ incrementQuantity.addEventListener("click", () => {
 
     if (parseInt(totalQuantity.innerText.toString()) < 3) {
         totalQuantity.innerText = (parseInt(totalQuantity.innerText.toString()) + 1).toString()
-        totalPrice.innerText =  "$" + (parseInt(totalQuantity.innerText) * 125).toString();
-        totalSelected.innerText = totalQuantity.innerText;
+       // totalPrice.innerText =  "$" + (parseInt(totalQuantity.innerText) * 125).toString();
+        // totalSelected.innerText = totalQuantity.innerText;
+
+
     }
 })
 
 decrementQuantity.addEventListener("click", () => {
     if (parseInt(totalQuantity.innerText.toString()) > 0) {
         totalQuantity.innerText = (parseInt(totalQuantity.innerText.toString()) - 1).toString()
-        totalPrice.innerText =  "$"+ (parseInt(totalQuantity.innerText) * 125).toString();
-        totalSelected.innerText = totalQuantity.innerText;
+       // totalSelected.innerText = totalQuantity.innerText;
+
+       
+
+     
     }
 })
 
@@ -51,9 +93,10 @@ cartBasket.addEventListener("click", () => {
         cartContainer.style.visibility = "hidden";
     }
     else {
-        console.log(totalQuantity.innerText.toString());
 
-        if(parseInt(totalQuantity.innerText.toString()) <= 0 )
+        console.log(totalItemsTip.innerText.toString());
+    
+        if(parseInt(totalItemsTip.innerText.toString()) <= 0 )
         {
             if(!filledCart.classList.contains("removed-content"))
             {
